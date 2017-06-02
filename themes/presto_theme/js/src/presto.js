f18288bd8d197ba54c7a8d45aefde03feb5f9fd8
Daniel Simington <daniel.simington@sitback.com.au>
2017-06-02 16:25:00 +1000

    theming for the landing page of the shop

--- themes/presto_theme/js/src/presto.js
+++ themes/presto_theme/js/src/presto.js
@@ -1,1 +1,21 @@
 'use strict';
+
+(function($, window, document, undefined) {
+
+  let f = {
+    addToCartInList: (el) => {
+      let $el = $(el.delegateTarget);
+      let $form = $el.parents('li.product-item').find('form').get(0);
+      if ($form) {
+        $form.submit();
+      }
+    }
+  }
+
+  $(document).ready( () => {
+    $('[data-action="cart"]').on('click', (el) => {
+      f.addToCartInList(el);
+    });
+  });
+
+})(jQuery, window, document);
