154a7367358b0e505b0f6ce0812175f84f672add
khadro <mordecai.chalk@gmail.com>
2017-06-07 13:28:46 +1000

    Feature/theme (#98)
    * added theming and template changes for promo bar object
    
    * removed extra decoration from button promo-bar
    
    * added new page structure, some theming for some images, and rewrote parts of the image template to simplify layout
    
    * added plugin to trigger lightboxes on images, rewrote images template for ability to output correctly, themed images blocks based on setup and missing/present information
    
    * #50: update field_image with field_media
    
    * Fix #65: update href for internal link
    
    * Updated gitignore to ignore all node_modules/bower_components dirs.
    
    * Ignore JS map files.
    
    * Added ES6 compilation support.
    
    * Added build-only task.
    
    * Improved gulpfile code format.
    
    * Removed manual lity installation and added via npm.
    
    * Removed base path, fixes #64.
    
    * theming articles and article lists, forms, social sharing and footer
    
    * added content to regions in config, and started theming commerce
    
    * amends to base structure styling to look better without bootstrap CDN
    
    * small theming bits for cart, changed jquery integration
    
    * fixed something added in by an errant bootstrap class
    
    * updated bootstrap variables and added some bs components
    
    * basic theming of cart page
    
    * Changed lity to be a Drupal library instead of an npm one.
    
    * Swapped to CDN version of lity.
    
    * theming the cart flow pages
    
    * fixed carousel default options
    
    * added mobile first column classes to page sections
    
    * #99: add configuration to setup and configure presto theme as default
    
    * Revert "#99: add configuration to setup and configure presto theme as default"
    
    This reverts commit 1b7983a343dd069370a5ad9b0fe286803ec1b0e2.
    
    * retheming presto using some rolled in NSWLS structure
    
    * theming for the landing page of the shop
    
    * theming thne commerce flow and admin areas
    
    * fixed up template issues after git pull added template files back in where they were not required
    
    * 101 - block padding fixes
    
    * added sub theme screenshot
    
    * shop listing template and base styling,
    form element styling
    
    * #98: update css to fix a last margin issue
    
    * navbar fixes
    
    * changes requested in pull request - removing comments mostly

--- themes/presto_theme/gulpfile.babel.js
+++ themes/presto_theme/gulpfile.babel.js
@@ -3,28 +3,34 @@
 import autoprefixer from 'gulp-autoprefixer';
 import browserSync from 'browser-sync';
 import clean from 'gulp-clean-css';
-import concat from 'gulp-concat';
 import gulp from 'gulp';
+import gutil from 'gulp-util';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import uglify from 'gulp-uglify';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import globby from 'globby';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import through from 'through2';
 
-const base = './';
+const OUTPUT_JS_FILE = 'presto.min.js';
 
 const src = {
-  js: `${base}/js/**/*.js`,
-  scss: `${base}/scss/**/*.scss`,
-  images: `${base}/images`,
-  fonts: `${base}/fonts`,
-  bootstrap: `${base}/node_modules/bootstrap-sass/assets/stylesheets`
+  js: 'js/src/**/*.js',
+  scss: 'scss/**/*.scss',
+  images: 'images',
+  fonts: 'fonts',
+  bootstrap: 'node_modules/bootstrap-sass/assets/stylesheets'
 };
 
 const dest = {
-  js: `${base}/js`,
-  css: `${base}/css`,
-  maps: `maps`,
-  images: `${base}/images`,
-  fonts: `${base}/fonts`
+  js: 'js/compiled',
+  css: 'css',
+  maps: 'maps',
+  images: 'images',
+  fonts: 'fonts'
 }
 
 
@@ -48,7 +54,6 @@ gulp.task('development', ['js', 'scss'], () => {
 
   gulp.watch(src.scss, ['scss']);
   gulp.watch(src.js, ['js']);
-  // gulp.watch(`${base}/**/*.php`, browserSync.reload);
 });
 
 
@@ -61,8 +66,11 @@ gulp.task('scss', () => {
       outputStyle: 'compressed',
       includePaths: [src.bootstrap]
     }))
-      .on('error', sass.logError)
-    .pipe(autoprefixer({ browsers: ['> 1%', 'IE 7'], cascade: false }))
+    .on('error', sass.logError)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'iOS 8'],
+      cascade: false
+    }))
     .pipe(clean())
     .pipe(sourcemaps.write(dest.maps))
     .pipe(gulp.dest(dest.css))
@@ -73,13 +81,38 @@ gulp.task('scss', () => {
 // Javascript
 // ------------------------------------------------------
 gulp.task('js', () => {
-  return gulp.src(src.js)
-    .pipe(concat('all.js'))
-    .pipe(uglify({mangle:false}))
+  let bundledStream = through();
+
+  bundledStream
+    .pipe(source(OUTPUT_JS_FILE))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify({
+      mangle: false
+    }))
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write(dest.maps))
     .pipe(gulp.dest(dest.js));
+
+  globby([src.js]).then((entries) => {
+    let bundler = browserify({
+      entries,
+      debug: true,
+      transform: [ babelify ]
+    })
+    // Drupal adds this in itself.
+    .ignore('jquery');
+
+    bundler.bundle().pipe(bundledStream);
+  }).catch((err) => {
+    bundledStream.emit('error', err);
+  });
+
+  return bundledStream;
 });
 
 
 // Tasks
 // ------------------------------------------------------
 gulp.task('default', ['development']);
+gulp.task('build', ['scss', 'js']);
